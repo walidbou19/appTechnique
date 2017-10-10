f5077b4a27b3053dc1a2b987834a005bfc6d5106
walidbou19 <32675507+walidbou19@users.noreply.github.com>
2017-10-10 15:43:57 +0200

    add package

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+var gulp = require('gulp')
+var sass = require('gulp-sass')
+var connect = require('gulp-connect')
+var browserify = require('browserify')
+var source = require('vinyl-source-stream')
+
+gulp.task('connect', function () {
+	connect.server({
+		root: 'public',
+		port: 4000
+	})
+})
+
+gulp.task('browserify', function() {
+	// Grabs the app.js file
+    return browserify('./app/app.js')
+    	// bundles it and creates a file called main.js
+        .bundle()
+        .pipe(source('main.js'))
+        // saves it the public/js/ directory
+        .pipe(gulp.dest('./public/js/'));
+})
+
+gulp.task('sass', function() {
+	return sass('sass/main.scss')
+		.pipe(gulp.dest('public/css'))
+})
+
+gulp.task('watch', function() {
+	gulp.watch('app/**/*.js', ['browserify'])
+	gulp.watch('sass/main.scss', ['sass'])
+})
+
+gulp.task('default', ['connect', 'watch'])
